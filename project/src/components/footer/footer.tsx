921155450c32a26109f8fb98383626a97a85ceca
Anastasia <tigra695@hotmail.com>
2022-07-15 09:34:07 +0300

    The 404 component has been added, routing settings have been applied

--- project/src/components/footer/footer.tsx
+++ project/src/components/footer/footer.tsx
@@ -1,12 +1,14 @@
+import {Link} from 'react-router-dom';
+
 function Footer(): JSX.Element {
   return (
     <footer className="page-footer">
       <div className="logo">
-        <a href="main.html" className="logo__link logo__link--light">
+        <Link to="/" className="logo__link logo__link--light">
           <span className="logo__letter logo__letter--1">W</span>
           <span className="logo__letter logo__letter--2">T</span>
           <span className="logo__letter logo__letter--3">W</span>
-        </a>
+        </Link>
       </div>
 
       <div className="copyright">
---
+++ project/src/const.ts
@@ -0,0 +1,14 @@
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Film = '/films/:id',
+  MyList = '/mylist',
+  AddReview = '/films/:id/review',
+  Player = '/player/:id'
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
